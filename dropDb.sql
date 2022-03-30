ea64f8675ab758a7017180de9717017ce25d903b
Ignacio Graffione <nachograffione@gmail.com>
2022-03-30 12:38:32 -0300

    Add sql code for deleting db

---
+++ dropDb.sql
@@ -0,0 +1,9 @@
+DROP INDEX fk_subcategory_category_index;
+DROP INDEX fk_pomo_category_index;
+DROP INDEX fk_pomo_subcategory_index;
+
+DROP TABLE pomo;
+DROP TABLE subcategory;
+DROP TABLE category;
+
+-- DROP DATABASE pomoRex;
