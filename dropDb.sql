e6ced28d90c3b867f842f6408c1479de0ac004f0
Ignacio Graffione <nachograffione@gmail.com>
2022-03-30 20:36:00 -0300

    Add trigger drops

--- dropDb.sql
+++ dropDb.sql
@@ -1,3 +1,6 @@
+﻿DROP TRIGGER trg_pomo_ins_upd ON pomo;
+DROP FUNCTION chk_pomo(); 
+
 DROP INDEX index_fk_subcategory_category;
 DROP INDEX index_fk_pomo_category;
 DROP INDEX index_fk_pomo_subcategory;
