05742b3124ca453547ae677a2732ea24a0a7e5a7
Ignacio Graffione <nachograffione@gmail.com>
2022-03-31 20:42:54 -0300

    Fix testing data

--- insertTestingData.sql
+++ insertTestingData.sql
@@ -1,4 +1,4 @@
------------------------------------------------------------------
+﻿-----------------------------------------------------------------
 -- Categories and Subcategories
 
 -- Cases:
@@ -11,29 +11,29 @@ INSERT INTO category
     VALUES (DEFAULT, 'Facultad');
 INSERT INTO subcategory
     VALUES (DEFAULT, 'Estudio',
-        (SELECT catID FROM category WHERE catName = 'Facultad'));
+        (SELECT catId FROM category WHERE catName = 'Facultad'));
 INSERT INTO subcategory
     VALUES (DEFAULT, 'TPs, Otros',
-        (SELECT catID FROM category WHERE catName = 'Facultad'));
+        (SELECT catId FROM category WHERE catName = 'Facultad'));
 
 -- Case 2
 INSERT INTO category
     VALUES (DEFAULT, 'Música');
 INSERT INTO subcategory
     VALUES (DEFAULT, 'Clases',
-        (SELECT catID FROM category WHERE catName = 'Música'));
+        (SELECT catId FROM category WHERE catName = 'Música'));
 INSERT INTO subcategory
     VALUES (DEFAULT, 'Ejercitar, tocar, componer, etc.',
-        (SELECT catID FROM category WHERE catName = 'Música'));
+        (SELECT catId FROM category WHERE catName = 'Música'));
 
 INSERT INTO category
     VALUES (DEFAULT, 'Ingeniería');
 INSERT INTO subcategory
     VALUES (DEFAULT, 'Clases',
-        (SELECT catID FROM category WHERE catName = 'Ingeniería'));
+        (SELECT catId FROM category WHERE catName = 'Ingeniería'));
 INSERT INTO subcategory
     VALUES (DEFAULT, 'Trabajo',
-        (SELECT catID FROM category WHERE catName = 'Ingeniería'));
+        (SELECT catId FROM category WHERE catName = 'Ingeniería'));
 
 -- Case 3
 INSERT INTO category
