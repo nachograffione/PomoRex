a39e3df56c08d80223c2a1133004c925f5a37387
Ignacio Graffione <nachograffione@gmail.com>
2022-04-01 00:59:30 -0300

    Substitute file constraintsTest with testsToFail

---
+++ constraintsTest.sql
@@ -0,0 +1,10 @@
+-----------------------------------------------------------------
+-- Constraints Tests (they should failed)
+
+-- Cases:
+--     1) Pomos that tries to break the constraints implemented with constraints
+--     2) Pomos that tries to break the constraints implemented with triggers
+
+-- Case 1
+
+-- Case 2
