b207989b062fde4cfe795f5da1d0aaf7702b3cfc
Ignacio Graffione <nachograffione@gmail.com>
2022-04-05 19:35:46 -0300

    Add start server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+// params
+const port = 3000;
+
+// start server
+const express = require("express");
+const app = express();
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
+
+// set EJS
+const path = require("path");
+app.set("view engine", "ejs");  //it will do require("ejs");
+app.set("views", path.join(__dirname, "views"));
