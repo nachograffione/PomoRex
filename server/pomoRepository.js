8d6f2bcc5150d681cff2d6caf5e89899a368d8c4
Ignacio Graffione <nachograffione@gmail.com>
2022-05-17 17:47:33 -0300

    Rearrange PomoRepository class

--- server/pomoRepository.js
+++ server/pomoRepository.js
@@ -1,23 +1,22 @@
-// sequelize init
-const { Sequelize, DataTypes } = require("sequelize");
-const sequelize = new Sequelize(
-    "postgres://postgres:postgres@localhost:5433/pomo_rex",
-    {
-        timestamps: true,
-        define: {
-            freezeTableName: true
-        }
-    }
-);
-// sequelize-auto init
-//      command line used to generate models: node_modules/.bin/sequelize-auto -o "./models" -d pomo_rex -h localhost -u postgres -p 5433 -x postgres -e postgres --caseFile p --caseModel p --caseProp l
+const { Sequelize, DataTypes, QueryTypes } = require("sequelize");
+// command line used to generate models imported here:
+// node_modules/.bin/sequelize-auto -o "./models" -d pomo_rex -h localhost -u postgres -p 5433 -x postgres -e postgres --caseFile p --caseModel p --caseProp l
 const initModels = require("./models/init-models");
-const models = initModels(sequelize);
-
 
 class PomoRepository {
     constructor() {
-
+        // sequelize init
+        this.sequelize = new Sequelize(
+            "postgres://postgres:postgres@localhost:5433/pomo_rex",
+            {
+                timestamps: true,
+                define: {
+                    freezeTableName: true
+                }
+            }
+        );
+        // sequelize-auto init
+        this.models = initModels(this.sequelize);
     }
 }
 
